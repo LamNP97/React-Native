47bdc0e1aad90e4b1d794ba5e1e448c69cf48cca
PhucLam1757 <lamphucnguyen05@gmail.com>
2022-05-14 22:57:20 +0700

    update title

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from "react-native";
+import { useState } from "react";
+import React from "react";
 
 export default function App() {
+  const [enteredGoal, setenteredGoal] = useState("");
+
+  const [addCourse, setaddCourse] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setenteredGoal(enteredText);
+  }
+
+  function addHandler() {
+    setaddCourse((currentCourse) => [...currentCourse, enteredGoal]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Scrol lView>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Enter your name"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="ADD" onPress={addHandler} />
+      </View>
+      <View>
+        {addCourse.map((goal) => (
+          <View key={goal} style={styles.styleList}>
+            <Text style={{color: 'white'}} >{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingHorizontal: 16,
+    paddingTop:50,
+    flex:1
+  },
+  inputContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+    marginBottom: 16,
+    borderBottomWidth:1,
+
+
+
+  },
+  inputBox: {
+    borderWidth: 1,
+    borderColor: "black",
+    width: "80%",
+    padding:10 ,
+  },
+
+  styleList: {
+    flex: 4,
+    padding: 10,
+    marginVertical:10,
+    color: "white",
+    backgroundColor: "#ccc",
+    borderRadius:6,
+    borderColor: "black",
+    backgroundColor: "#5e0acc",
+    borderWidth: 1,
   },
 });
